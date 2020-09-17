ec063c6b16c57312ae776fc38e13b8a048ae4520
Guilherme Souza <ogrsouza@gmail.com>
2020-09-17 07:12:47 -0300

    feat: add Lock type

---
+++ Sources/PancakeCore/Utilities/Lock.swift
@@ -0,0 +1,119 @@
+import Foundation
+
+/// `Lock` exposes `os_unfair_lock` on supported platforms, with pthread mutex as the fallback (or for recursive locks).
+public class Lock {
+
+    final class UnfairLock: Lock {
+        private let _lock: os_unfair_lock_t
+
+        override init() {
+            _lock = .allocate(capacity: 1)
+            _lock.initialize(to: os_unfair_lock())
+            super.init()
+        }
+
+        override func lock() {
+            os_unfair_lock_lock(_lock)
+        }
+
+        override func unlock() {
+            os_unfair_lock_unlock(_lock)
+        }
+
+        override func `try`() -> Bool {
+            return os_unfair_lock_trylock(_lock)
+        }
+
+        deinit {
+            _lock.deinitialize(count: 1)
+            _lock.deallocate()
+        }
+    }
+
+    final class PthreadLock: Lock {
+        private let _lock: UnsafeMutablePointer<pthread_mutex_t>
+
+        init(recursive: Bool = false) {
+            _lock = .allocate(capacity: 1)
+            _lock.initialize(to: pthread_mutex_t())
+
+            let attr = UnsafeMutablePointer<pthread_mutexattr_t>.allocate(capacity: 1)
+            attr.initialize(to: pthread_mutexattr_t())
+            pthread_mutexattr_init(attr)
+
+            defer {
+                pthread_mutexattr_destroy(attr)
+                attr.deinitialize(count: 1)
+                attr.deallocate()
+            }
+
+            pthread_mutexattr_settype(attr, Int32(recursive ? PTHREAD_MUTEX_RECURSIVE : PTHREAD_MUTEX_ERRORCHECK))
+
+            let status = pthread_mutex_init(_lock, attr)
+            assert(status == 0, "Unexpected pthread mutex error code: \(status)")
+
+            super.init()
+        }
+
+        override func lock() {
+            let status = pthread_mutex_lock(_lock)
+            assert(status == 0, "Unexpected pthread mutex error code: \(status)")
+        }
+
+        override func unlock() {
+            let status = pthread_mutex_unlock(_lock)
+            assert(status == 0, "Unexpected pthread mutex error code: \(status)")
+        }
+
+        override func `try`() -> Bool {
+            let status = pthread_mutex_trylock(_lock)
+            switch status {
+            case 0:
+                return true
+            case EBUSY, EAGAIN:
+                return false
+            default:
+                assertionFailure("Unexpected pthread mutex error code: \(status)")
+                return false
+            }
+        }
+
+        deinit {
+            let status = pthread_mutex_destroy(_lock)
+            assert(status == 0, "Unexpected pthread mutex error code: \(status)")
+
+            _lock.deinitialize(count: 1)
+            _lock.deallocate()
+        }
+    }
+
+    /// Return an instance of a `Lock`, according to API availability (`os_unfair_lock_t` or `pthread_mutex_t` based).
+    ///
+    /// - returns: a `Lock` instance
+    public static func make() -> Lock {
+        if #available(*, iOS 10.0) {
+            return UnfairLock()
+        }
+
+        return PthreadLock()
+    }
+
+    private init() {}
+
+    /// Locks the lock
+    public func lock() { fatalError("Missing Implementation") }
+
+    /// Unlocks the lock
+    public func unlock() { fatalError("Missing Implementation") }
+
+    /// Locks the lock if it is not already locked.
+    ///
+    /// - Returns: Returns `true` if the lock was succesfully locked and `false` if the lock was already locked.
+    public func `try`() -> Bool { fatalError("Missing Implementation") }
+
+    public final func around<Result>(_ block: () throws -> Result) rethrows -> Result {
+        lock()
+        defer { unlock() }
+        return try block()
+    }
+}
