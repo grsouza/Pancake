0c0f665f54a5b82093b3c047316ed3ff372b6bd1
Guilherme Souza <ogrsouza@gmail.com>
2020-09-17 07:12:17 -0300

    feat: add extension for bridging String and NSString

---
+++ Sources/PancakeCore/Extensions/String.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+extension String {
+
+    /// Creates a string from the `dump` output of the given value.
+    public init<T>(dumping value: T) {
+        self.init()
+        dump(value, to: &self)
+    }
+
+    public var nsString: NSString { self as NSString }
+
+    public func substring(with nsRange: NSRange) -> String {
+        nsString.substring(with: nsRange)
+    }
+}
