b3eff6d3e5cd02e57e41f45faf8d95a4c3d713df
Guilherme Souza <ogrsouza@gmail.com>
2020-10-28 07:21:50 -0300

    Add VStack test using snapshot testing

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
   name: "Pancake",
   platforms: [
-    .iOS(.v10),
+    .iOS(.v11),
     .macOS(.v10_11),
   ],
   products: [
---
+++ Tests/PancakeUITests/VStackTests.swift
@@ -0,0 +1,27 @@
+import PancakeUI
+import XCTest
+import SnapshotTesting
+
+final class VStackTests: XCTestCase {
+
+    func testVStack() {
+        let views = (0..<10).map { _ in
+            UIView().with {
+                $0.height(32)
+                $0.backgroundColor = .darkGray
+            }
+        }
+
+        let stack = VStack(
+            spacing: 10,
+            alignment: .fill,
+            distribution: .fill,
+            views
+        ).with {
+            $0.width(256)
+            $0.backgroundColor = .white
+        }
+
+        assertSnapshot(matching: stack, as: .image)
+    }
+}
