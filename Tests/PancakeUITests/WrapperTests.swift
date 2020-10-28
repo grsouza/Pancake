3e9040d8996b772852db32b2d7eb62558419f98c
Guilherme Souza <ogrsouza@gmail.com>
2020-10-28 08:16:24 -0300

    Add tests for Wrapper

---
+++ Tests/PancakeUITests/WrapperTests.swift
@@ -0,0 +1,41 @@
+import PancakeUI
+import SnapshotTesting
+import UIKit
+import XCTest
+
+final class WrapperTests: XCTestCase {
+
+  func testWrapperTopAlignment() {
+    let view = UIView().with {
+      $0.backgroundColor = .darkGray
+      $0.size(CGSize(width: 32, height: 32))
+    }
+
+    let alignments: [Wrapper.Alignment] = [
+      .top,
+      .trailing,
+      .bottom,
+      .leading,
+      .centerX,
+      .centerY,
+      [.top, .leading],
+      [.top, .trailing],
+      [.bottom, .leading],
+      [.bottom, .trailing],
+      [.centerX, .top],
+      [.centerX, .bottom],
+      [.centerY, .leading],
+      [.centerY, .trailing],
+    ]
+
+    for alignment in alignments {
+      let wrapper = Wrapper(view, alignment: alignment).with {
+        $0.size(CGSize(width: 64, height: 64))
+        $0.backgroundColor = .white
+      }
+
+      assertSnapshot(matching: wrapper, as: .image)
+    }
+  }
+
+}
