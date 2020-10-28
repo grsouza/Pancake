bd298c9e3984639afa960a2989fde7699498e075
Guilherme Souza <ogrsouza@gmail.com>
2020-10-28 07:24:58 -0300

    Import UIKit

--- Tests/PancakeUITests/VStackTests.swift
+++ Tests/PancakeUITests/VStackTests.swift
@@ -1,27 +1,28 @@
 import PancakeUI
-import XCTest
 import SnapshotTesting
+import UIKit
+import XCTest
 
 final class VStackTests: XCTestCase {
 
-    func testVStack() {
-        let views = (0..<10).map { _ in
-            UIView().with {
-                $0.height(32)
-                $0.backgroundColor = .darkGray
-            }
-        }
-
-        let stack = VStack(
-            spacing: 10,
-            alignment: .fill,
-            distribution: .fill,
-            views
-        ).with {
-            $0.width(256)
-            $0.backgroundColor = .white
-        }
-
-        assertSnapshot(matching: stack, as: .image)
+  func testVStack() {
+    let views = (0..<10).map { _ in
+      UIView().with {
+        $0.height(32)
+        $0.backgroundColor = .darkGray
+      }
     }
+
+    let stack = VStack(
+      spacing: 10,
+      alignment: .fill,
+      distribution: .fill,
+      views
+    ).with {
+      $0.width(256)
+      $0.backgroundColor = .white
+    }
+
+    assertSnapshot(matching: stack, as: .image)
+  }
 }
