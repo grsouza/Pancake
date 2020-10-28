82b360da49fdf300897b9db958f2b2fd1a5259a7
Guilherme Souza <ogrsouza@gmail.com>
2020-10-28 07:02:50 -0300

    Add Wrapper view

---
+++ Sources/PancakeUI/Wrapper.swift
@@ -0,0 +1,57 @@
+#if canImport(UIKit)
+import UIKit
+
+public final class Wrapper: View {
+
+  // MARK: Lifecycle
+
+  public init(_ view: UIView, alignment: Alignment = .all, usingSafeArea: Bool = false) {
+    super.init()
+
+    addSubview(view)
+
+    if alignment.contains(.top) {
+      view.topToSuperview(usingSafeArea: usingSafeArea)
+    }
+
+    if alignment.contains(.trailing) {
+      view.trailingToSuperview(usingSafeArea: usingSafeArea)
+    }
+
+    if alignment.contains(.bottom) {
+      view.bottomToSuperview(usingSafeArea: usingSafeArea)
+    }
+
+    if alignment.contains(.leading) {
+      view.leadingToSuperview(usingSafeArea: usingSafeArea)
+    }
+
+    if alignment.contains(.centerX) {
+      view.centerXToSuperview(usingSafeArea: usingSafeArea)
+    }
+
+    if alignment.contains(.centerY) {
+      view.centerYToSuperview(usingSafeArea: usingSafeArea)
+    }
+  }
+
+  // MARK: Public
+
+  public struct Alignment: OptionSet {
+    public let rawValue: UInt8
+
+    public init(rawValue: UInt8) {
+      self.rawValue = rawValue
+    }
+
+    public static let top = Alignment(rawValue: 1 << 0)
+    public static let trailing = Alignment(rawValue: 1 << 1)
+    public static let bottom = Alignment(rawValue: 1 << 2)
+    public static let leading = Alignment(rawValue: 1 << 3)
+    public static let centerX = Alignment(rawValue: 1 << 4)
+    public static let centerY = Alignment(rawValue: 1 << 5)
+    public static let all: Alignment = [.top, .trailing, .bottom, .leading, .centerX, .centerY]
+  }
+
+}
+#endif
