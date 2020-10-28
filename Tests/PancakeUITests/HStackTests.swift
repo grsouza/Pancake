ccec08cd2c9f8fceeaf5d2e7b37f4de392952211
Guilherme Souza <ogrsouza@gmail.com>
2020-10-28 08:01:42 -0300

    Add tests for HStack

---
+++ Tests/PancakeUITests/HStackTests.swift
@@ -0,0 +1,63 @@
+import PancakeUI
+import SnapshotTesting
+import UIKit
+import XCTest
+
+final class HStackTests: XCTestCase {
+
+  func testHStack() {
+    let views = (0..<10).map { _ in
+      UIView().with {
+        $0.width(32)
+        $0.backgroundColor = .darkGray
+      }
+    }
+
+    let stack = HStack(
+      spacing: 10,
+      alignment: .fill,
+      distribution: .fill,
+      views
+    ).with {
+      $0.height(32)
+      $0.backgroundColor = .white
+    }
+
+    assertSnapshot(matching: stack, as: .image)
+  }
+
+  func testHStackCustomAlignment() {
+    let view1 = UIView().with {
+      $0.height(100)
+      $0.width(32)
+      $0.backgroundColor = .darkGray
+    }
+
+    let view2 = UIView().with {
+      $0.height(80)
+      $0.width(32)
+      $0.backgroundColor = .darkGray
+    }
+
+    let view3 = UIView().with {
+      $0.height(120)
+      $0.width(32)
+      $0.backgroundColor = .darkGray
+    }
+
+    let alignments: [UIStackView.Alignment] = [.top, .center, .bottom]
+
+    for alignment in alignments {
+      let stack = HStack(
+        spacing: 10,
+        alignment: alignment,
+        view1, view2, view3
+      ).with {
+        $0.height(128)
+        $0.backgroundColor = .white
+      }
+
+      assertSnapshot(matching: stack, as: .image)
+    }
+  }
+}
