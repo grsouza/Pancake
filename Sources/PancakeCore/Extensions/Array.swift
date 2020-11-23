99a46d9369ae4e32517abd595d30d6effdbdce96
Guilherme Souza <ogrsouza@gmail.com>
2020-11-22 21:07:45 -0300

    Add inlinable to array extensions

--- Sources/PancakeCore/Extensions/Array.swift
+++ Sources/PancakeCore/Extensions/Array.swift
@@ -5,7 +5,14 @@ extension Array {
   /// - Parameter newElement: Element to insert
   ///
   /// Complexity: O(n), where n is the length of the collection.
+  @inlinable
   public mutating func prepend(_ newElement: Element) {
     insert(newElement, at: 0)
   }
+
+  @inlinable
+  public mutating func prepend<C>(contentsOf newElements: C) where C: Collection, C.Element == Element {
+    insert(contentsOf: newElements, at: 0)
+  }
+
 }
