370eb38b9074d06314dbab3dd3f511b2ec2f6976
Guilherme Souza <ogrsouza@gmail.com>
2021-02-08 06:52:10 -0300

    feat: wrapper around pthread_rwlock_t

---
+++ Sources/PancakeCore/Utilities/RWLock.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//
+//
+//  Created by Guilherme Souza on 27/01/21.
+//
+
+import Foundation
+
+public final class RWLock {
+
+  // MARK: Lifecycle
+
+  public init() {
+    lock = UnsafeMutablePointer.allocate(capacity: 1)
+    let status = pthread_rwlock_init(lock, nil)
+    assert(status == 0)
+  }
+
+  deinit {
+    let status = pthread_rwlock_destroy(lock)
+    assert(status == 0)
+    lock.deinitialize(count: 1)
+    lock.deallocate()
+  }
+
+  // MARK: Public
+
+  public func read<Result>(_ block: () throws -> Result) rethrows -> Result {
+    pthread_rwlock_rdlock(lock)
+    defer { pthread_rwlock_unlock(lock) }
+    return try block()
+  }
+
+  public func write<Result>(_ block: () throws -> Result) rethrows -> Result {
+    pthread_rwlock_wrlock(lock)
+    defer { pthread_rwlock_unlock(lock) }
+    return try block()
+  }
+
+  // MARK: Private
+
+  private var lock: UnsafeMutablePointer<pthread_rwlock_t>
+
+}
