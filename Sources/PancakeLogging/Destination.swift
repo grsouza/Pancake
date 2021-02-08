c53c74506ffe83811c99b563b2ad4d28ecea009e
Guilherme Souza <ogrsouza@gmail.com>
2021-02-08 06:53:10 -0300

    style: format code

--- Sources/PancakeLogging/Destination.swift
+++ Sources/PancakeLogging/Destination.swift
@@ -12,28 +12,28 @@ public protocol Destination {
   )
 }
 extension Destination {
-    /// Returns only the file name without the extension.
-    func fileName(for file: String) -> String {
-      file.components(separatedBy: "/").last?.components(separatedBy: ".").first ?? ""
-    }
+  /// Returns only the file name without the extension.
+  func fileName(for file: String) -> String {
+    file.components(separatedBy: "/").last?.components(separatedBy: ".").first ?? ""
+  }
 
-    func emoji(for level: Logger.Level) -> String {
-      switch level {
-      case .verbose: return "📝"
-      case .debug: return "✅"
-      case .info: return "ℹ️"
-      case .warning: return "⚠️"
-      case .error: return "🚫"
-      }
+  func emoji(for level: Logger.Level) -> String {
+    switch level {
+    case .verbose: return "📝"
+    case .debug: return "✅"
+    case .info: return "ℹ️"
+    case .warning: return "⚠️"
+    case .error: return "🚫"
     }
+  }
 
-    func levelString(for level: Logger.Level) -> String {
-      switch level {
-      case .verbose: return "VERBOSE"
-      case .debug: return "DEBUG"
-      case .info: return "INFO"
-      case .warning: return "WARNING"
-      case .error: return "ERROR"
-      }
+  func levelString(for level: Logger.Level) -> String {
+    switch level {
+    case .verbose: return "VERBOSE"
+    case .debug: return "DEBUG"
+    case .info: return "INFO"
+    case .warning: return "WARNING"
+    case .error: return "ERROR"
     }
+  }
 }
